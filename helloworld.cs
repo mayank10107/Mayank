6fbab83663d20b9b162185de7f528263769ed7a7
mayank10107 <46420727+mayank10107@users.noreply.github.com>
2019-03-13 11:48:11 +0900

    Update helloworld.cs

--- helloworld.cs
+++ helloworld.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");;
         }
     }
 }
